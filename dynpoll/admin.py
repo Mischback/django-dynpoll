26b99c4cad19addc79a7f59b9e01379310997d72
Mischback <dev@mischback.de>
2018-05-15 17:14:25 +0200

    Added 'Vote' model

--- dynpoll/admin.py
+++ dynpoll/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from dynpoll.models import Choice, Question
+from dynpoll.models import Choice, Question, Vote
 
 
 admin.site.register(Choice)
 admin.site.register(Question)
+admin.site.register(Vote)
--- dynpoll/models.py
+++ dynpoll/models.py
@@ -11,7 +11,14 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
+
+
+class Vote(models.Model):
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    from_ip = models.GenericIPAddressField()
+
+    def __str__(self):
+        return 'Vote on [Choice {}] from {}'.format(self.choice.pk, self.from_ip)
