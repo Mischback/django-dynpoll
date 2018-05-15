a6837fa73a89817ad121ead5b7044989c4b81bae
Mischback <dev@mischback.de>
2018-05-15 16:24:43 +0200

    Made app work with 'tox' and 'make'

---
+++ tests/utils/urls_dev.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Provides a minimal URL configuration for tests and development.
+
+This file mimics a project's URL configuration, in order to run the app during
+development and testing.
+
+TODO: create a 'root-view' under the URL '/', which renders some basic template."""
+
+# Django imports
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
