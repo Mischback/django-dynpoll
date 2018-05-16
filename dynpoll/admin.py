15313bd09ead04813cd902a73368b77753c6074c
Mischback <dev@mischback.de>
2018-05-16 07:47:51 +0200

    Created QuestionSequence

--- dynpoll/admin.py
+++ dynpoll/admin.py
@@ -2,9 +2,11 @@
 
 from django.contrib import admin
 
-from dynpoll.models import Choice, Question, Vote
+from dynpoll.models import Choice, Question, QuestionSequence, QuestionSequenceItem, Vote
 
 
 admin.site.register(Choice)
 admin.site.register(Question)
 admin.site.register(Vote)
+admin.site.register(QuestionSequence)
+admin.site.register(QuestionSequenceItem)
--- dynpoll/models.py
+++ dynpoll/models.py
@@ -24,3 +24,22 @@ class Vote(models.Model):
 
     def __str__(self):
         return 'Vote on [Choice {}] from {}'.format(self.choice.pk, self.from_ip)
+
+
+class QuestionSequence(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class QuestionSequenceItem(models.Model):
+    sequence = models.ForeignKey(QuestionSequence, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.SET_NULL, null=True)
+    order = models.SmallIntegerField(default=1)
+
+    class Meta:
+        unique_together = ('sequence', 'question')
+
+    def __str__(self):
+        return '[{}] {} - {}'.format(self.sequence, self.order, self.question)
